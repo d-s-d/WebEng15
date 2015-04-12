0cd71a5a7a48ed7ddbdfcf5c7954963bc690caa6
Stefan Dietiker <stefand@ethz.ch>
2015-04-12 12:11:12 +0200

    adjusted templates

--- ex03/htdocs/wp-content/themes/LaraJade/functions.php
+++ ex03/htdocs/wp-content/themes/LaraJade/functions.php
@@ -10,4 +10,17 @@
      echo $ssurl.'/'.$locurl;
  }
 
+ add_option('introduction_text', '', '', 'no');
+ /*
+// the following was used for testing purposes when the dashboard widget was not ready
+
+ update_option('introduction_text',"Lara is a freelance web designer with satisfied clients worldwide. Lara has been designing websites professionally for over eight years, and is very keen on personal service. I would describe Lara as a spiritual fast learning generalist with a passion for remarkable design.
+					
+That´s enough of talking about myself in the third person.
+
+At first I intended to be an animator and went to design school fully motivated to become just that. One thing led to another and 2 years went by and I was a (almost) fully fledged freelance web designer without ever planning to become one.
+
+I have sucessfully been a freelance web designer now for a while and it has given me even more love for this work. I have worked with ad agencies, web developers, diaper makers, pension funds, furniture makers, businiess women & men, friends & family.
+");
+	*/
 ?>
